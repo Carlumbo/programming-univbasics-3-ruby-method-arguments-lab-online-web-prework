34db47ecce834223ec1a6971f46588115efd563a
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-08 20:45:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,9 @@ def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
-def introduction_with_language_optional(name)
+def introduction_with_language_optional(name, language)
+  if language == nil 
+    ptus
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
   if introduction_with_language_optional(name)
     puts
